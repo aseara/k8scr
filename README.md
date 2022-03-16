8e871ed5c111db25fd14a2c2b005dffe69f112f7
aseara <aseara@163.com>
2022-03-16 19:27:38 +0800

    调整项目结构

--- README.md
+++ README.md
@@ -4,13 +4,12 @@
 
 ```
 
-ROOT_PACKAGE="github.com/aseara/k8scr"
-CUSTOM_RESOURCE_NAME="samplecrd"
-CUSTOM_RESOURCE_VERSION="v1"
+ROOT_PACKAGE="github.com/aseara/k8scr/internal/api"
+PACKAGE_VERSION="samplecrd:v1"
 
-~/go/pkg/mod/k8s.io/code-generator@v0.23.4/generate-groups.sh all "pkg/client" "$ROOT_PACKAGE/pkg/apis" "$CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION" --go-header-file hack/custom-boilerplate.go.txt
+~/go/pkg/mod/k8s.io/code-generator@v0.23.4/generate-groups.sh all "internal/client" "$ROOT_PACKAGE" "$PACKAGE_VERSION" --go-header-file hack/custom-boilerplate.go.txt
 
-mv -f $ROOT_PACKAGE/pkg/apis/$CUSTOM_RESOURCE_NAME/$CUSTOM_RESOURCE_VERSION/*.* pkg/apis/$CUSTOM_RESOURCE_NAME/$CUSTOM_RESOURCE_VERSION/
+mv -f $ROOT_PACKAGE/${PACKAGE_VERSION/://}/*.* internal/api/${PACKAGE_VERSION/://}/
 rm -rf github.com
 
 ```
---
+++ internal/apis/samplecrd/v1/register.go
@@ -0,0 +1,49 @@
+package v1
+
+import (
+	"github.com/aseara/k8scr/internal/apis/samplecrd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// SchemeGroupVersion is the identifier for the API which includes
+// the name of the group and the version of the API
+var SchemeGroupVersion = schema.GroupVersion{
+	Group:   samplecrd.GroupName,
+	Version: samplecrd.Version,
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// create a SchemeBuilder which uses functions to add types to
+// the scheme
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+// addKnownTypes adds our types to the API scheme by registering
+// Network and NetworkList
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
+		&Network{},
+		&NetworkList{},
+	)
+
+	// register the type in the scheme
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
